21ab660b7a27ba93e1114cf67e4a8c6a6727417b
Chologalactico <116129679+Chologalactico@users.noreply.github.com>
2023-09-21 09:15:09 -0500

    Delete stb directory

--- stb/src/main/java/co/edu/usbcali/stb/controller/SucursalController.java
+++
@@ -1,4 +0,0 @@
-package co.edu.usbcali.stb.controller;
-
-public class SucursalController {
-}
--- stb/src/main/java/co/edu/usbcali/stb/repository/Clienterepository.java
+++
@@ -1,9 +0,0 @@
-package co.edu.usbcali.stb.repository;
-
-import co.edu.usbcali.stb.domain.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Clienterepository extends JpaRepository<Cliente,Integer> {
-}
